fd561c1842054f773966f92ef41ec82da977859e
/install.sh <1745631475@qq.com>
2021-08-26 14:56:17 +0800

    添加单例模式基类

---
+++ Assets/Htool/HFramework/SingletonBaseClass/Singleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Htool
+{
+    /// <summary>
+    /// 单例模式基类（不继承Mono）
+    /// </summary>
+    public class Singleton<T> where T : class, new()
+    {
+        private static T instance;
+        public static T GetInstance { get { return instance ?? (instance = new T()); } }
+    }
+}
---
+++ Assets/Htool/HFramework/SingletonBaseClass/SingletonAutoMono.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Htool
+{
+    public class SingletonAutoMono : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
